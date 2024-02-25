d73eb69f4255695ab2a6461ab018dd1b5ce54a51
CausedEight <mslava014@gmail.com>
2024-02-25 12:10:30 -0800

    first commit

---
+++ Berkeley_2024/Berkeley_2024/Berkeley_2024App.swift
@@ -0,0 +1,17 @@
+//
+//  Berkeley_2024App.swift
+//  Berkeley_2024
+//
+//  Created by Slava Miroshnichenko on 2/25/24.
+//
+
+import SwiftUI
+
+@main
+struct Berkeley_2024App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
